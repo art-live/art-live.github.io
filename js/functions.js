982cc015be9c7be35dc025c2e5d73853dec6e9d9
art-live <30608552+art-live@users.noreply.github.com>
2017-08-20 10:28:22 +0300

    art-live mega up!
    art-live mega up!

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-(function($) {
+
   var $li = $('.img-list').find('> li'),
       $links = $li.find('> a'),
       $lightbox = $('.lightbox'),
@@ -8,62 +8,44 @@
       liIndex,
       targetImg;
   
-  //preload images
-  var imgSources = [
-  'images/img-1-lg.jpg',
-  'images/img-2-lg.jpg',
-  'images/img-3-lg.jpg',
-  'images/img-4-lg.jpg'
-  ];
 
-  var imgs = [];
-  for (var i = 0; i < imgSources.length; i++) {
-    imgs[i] = new Image();
-    imgs[i].src = imgSources[i];
-  }
-
-  function replaceImg(src) {
-    $lightbox.find('img').attr('src', src);
-  }
-
-  function getHref(index) {
-    return $li.eq(index).find('>a').attr('href');
-  }
-
-  function closeLigtbox() {
-    $lightbox.fadeOut();
-  }
-
-  $overlay.click(closeLigtbox);
 
   $links.click(function(e) {
     e.preventDefault();
-    targetImg = $(this).attr('href');
     liIndex = $(this).parent().index();
-    replaceImg(targetImg);
-    $lightbox.fadeIn();
+    targetImg = $(this).attr('href');
+    $lightbox.find('.image_slide').attr('src', targetImg)
+    $lightbox.fadeIn(500);
   });
 
-   $next.click( function() {  
-    if ( (liIndex + 1) < $li.length ) { 
-      targetImg = getHref(liIndex + 1);
-      liIndex ++;
-    } else {
-      targetImg = getHref(0);
-      liIndex = 0;
-    }
-    replaceImg(targetImg);
-  });
 
-   $prev.click( function() {  
-    if ( (liIndex) > 0 ) { 
-      targetImg = getHref(liIndex - 1);
-      liIndex --;
-    } else {
-      targetImg = getHref($li.length - 1);
-      liIndex = $li.length - 1;
-    }
-    replaceImg(targetImg);
-  });
-  
-})(jQuery);
+$overlay.click(function() {
+	$('.lightbox').fadeOut(
+		)
+});
+
+$next.click(function() {
+	if((liIndex + 1) < $li.length ){
+		targetImg = $li.eq(liIndex + 1).find('> a').attr('href');
+		liIndex ++;
+
+	} else{
+		targetImg = $li.eq(0).find('> a').attr('href');
+		liIndex = 0;
+	}
+	 $lightbox.find('.image_slide').attr('src', targetImg)
+});
+
+$prev.click(function() {
+	if(liIndex > 0){
+		targetImg = $li.eq(liIndex - 1).find('> a').attr('href');
+		liIndex --;
+
+	} else{
+		targetImg = $li.eq(liIndex - 1).find('> a').attr('href');
+		liIndex = 11;
+	}
+	 $lightbox.find('.image_slide').attr('src', targetImg)
+})
+
+
